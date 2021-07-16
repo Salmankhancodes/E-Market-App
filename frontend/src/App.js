d03de80575d3f78434975c64f938e6b3f97b0c8e
Salmankhancodes <117salmankhan@gmail.com>
2021-07-16 13:08:07 +0530

    Header,Footer and React Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-
-function App() {
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+const App = () => {
   return (
     <>
-    <h1>Welcome to ProShop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container >
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
