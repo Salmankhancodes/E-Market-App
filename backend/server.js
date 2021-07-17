c0edb626c3bc4c9b79ed7825630e110dee935af3
Salmankhancodes <117salmankhan@gmail.com>
2021-07-17 14:12:19 +0530

    Server Products from backend

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const products = require('./data/products')
+
+app.get('/', (req, res) => {
+  res.json('Api is running')
+})
+
+app.get('/products', (req, res) => {
+  res.json(products)
+})
+
+app.get('/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(product)
+})
+
+app.listen(5000, console.log('Server is running on port 5000'))
