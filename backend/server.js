f9d5364fa9684620d51877753c7028213a53f3f9
Salmankhancodes <117salmankhan@gmail.com>
2021-07-18 02:18:36 +0530

    Connected Application to database

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ import express from 'express'
 const app = express()
 import products from './data/products.js'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import colors from 'colors'
 
 dotenv.config()
+connectDB()
 
 app.get('/', (req, res) => {
   res.json('Api is running...')
@@ -23,6 +26,7 @@ const PORT = process.env.PORT || 5000
 app.listen(
   PORT,
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
   )
 )
