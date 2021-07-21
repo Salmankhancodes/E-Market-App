2021dd3c534565855db7a5043ca11f5cee47d6d3
Salmankhancodes <117salmankhan@gmail.com>
2021-07-21 21:34:20 +0530

    User register reducer, action & screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
 const App = () => {
   return (
     <Router>
@@ -13,6 +14,8 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Route path='/login' component={LoginScreen} />
+
+          <Route path='/register' component={RegisterScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/' component={HomeScreen} exact />
