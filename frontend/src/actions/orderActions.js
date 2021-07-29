3a20120a4c8ed8f172a946bd9890c6271358a312
Salmankhancodes <117salmankhan@gmail.com>
2021-07-29 17:11:56 +0530

    Added Paypal Payment Gateway

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -77,25 +77,41 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
   }
 }
 
-export const orderPayReducer = (state = {}, action) => {
-  switch (action.type) {
-    case ORDER_PAY_REQUEST:
-      return {
-        loading: true,
+export const payOrder =
+  (orderId, paymentResult) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: ORDER_PAY_REQUEST,
+      })
+
+      const {
+        userLogin: { userInfo },
+      } = getState()
+
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${userInfo.token}`,
+        },
       }
-    case ORDER_PAY_SUCCESS:
-      return {
-        loading: false,
-        success: true,
-      }
-    case ORDER_PAY_FAIL:
-      return {
-        loading: false,
-        error: action.payload,
-      }
-    case ORDER_PAY_RESET:
-      return {}
-    default:
-      return state
+
+      const { data } = await axios.put(
+        `/api/orders/${orderId}/pay`,
+        paymentResult,
+        config
+      )
+
+      dispatch({
+        type: ORDER_PAY_SUCCESS,
+        payload: data,
+      })
+    } catch (error) {
+      dispatch({
+        type: ORDER_PAY_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      })
+    }
   }
-}
